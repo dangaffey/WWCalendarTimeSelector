b09a9974ad848a11a2f7e6e525b7db3585609d00
Daniel Gaffey <danielgaffey@Daniels-MBP.fios-router.home>
2018-05-14 20:35:39 -0400

    Updates

---
+++ WWCalendarTimeSelector-iOS/WWCalendarTimeSelector_iOS.h
@@ -0,0 +1,19 @@
+//
+//  WWCalendarTimeSelector_iOS.h
+//  WWCalendarTimeSelector-iOS
+//
+//  Created by Daniel Gaffey on 5/14/18.
+//  Copyright © 2018 Wonder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WWCalendarTimeSelector_iOS.
+FOUNDATION_EXPORT double WWCalendarTimeSelector_iOSVersionNumber;
+
+//! Project version string for WWCalendarTimeSelector_iOS.
+FOUNDATION_EXPORT const unsigned char WWCalendarTimeSelector_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WWCalendarTimeSelector_iOS/PublicHeader.h>
+
+
